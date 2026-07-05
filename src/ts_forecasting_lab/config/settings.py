e7fb859cfa43799b9c910d54c6b88e1ac8a42e6e
Sumit Singh <sumit20@gmail.com>
2026-07-05 15:45:06 +0530

    Finish settings.py

---
+++ src/ts_forecasting_lab/config/settings.py
@@ -0,0 +1,38 @@
+"""
+Application Settings.
+
+This module loads the **application configuration** from config.yaml.
+Validates it using the Pydantic Schema, and exposes a single validated Settings object for 
+use throughout the application.
+"""
+
+from pathlib import Path
+from ts_forecasting_lab.config.reader import load_yaml_config
+from ts_forecasting_lab.config.schema import Settings
+
+def get_settings(config_path: Path | None = None) -> Settings:
+    """ 
+    Load and Validate Application Settings
+    
+    Parameters
+    -----------
+    config_path
+        Optional path to a configuration file.
+        If omitted, the default config/config.yaml is used.
+        
+    Returns
+    ---------
+    Settings
+        A validated Settings object.
+    """
+    
+    if config_path is None:
+        config_path = Path("config/config.yaml")
+        
+    raw_config = load_yaml_config(config_path)
+    
+    settings = Settings.model_validate(raw_config)
+    
+    return settings
+
+settings = get_settings()
