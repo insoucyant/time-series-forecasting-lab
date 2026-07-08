de1b73da2359983024d9b213fe0ab2fd77243e0b
Sumit Singh <sumit20@gmail.com>
2026-07-08 14:05:50 +0530

    Update base.py

---
+++ src/ts_forecasting_lab/models/base.py
@@ -0,0 +1,9 @@
+""" 
+Base forecasting interfaces.
+
+This module defines the core abstractions used by all forecasting models.
+
+Every forecasting model in the repository should inherit from BaseForecaster 
+and return ForecastResult from predict(). 
+"""
+
